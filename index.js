48d1b3deaa0fd3a6a13d9b59f0ea49314c833304
Tam2407 <tam_dpm225470@student.agu.edu.vn>
2026-04-08 20:01:02 +0700

    cập nhật logo

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ taoAdminDauTien(); // Chạy hàm này khi server khởi động
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
